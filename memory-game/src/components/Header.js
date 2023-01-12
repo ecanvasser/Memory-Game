8c627da8cd1d93049435755fe6c4c7ad5124e842
ecanvasser <eddiecanvassere9@gmail.com>
2023-01-11 23:10:35 -0800

    Add header icon

--- memory-game/src/components/Header.js
+++ memory-game/src/components/Header.js
@@ -1,9 +1,14 @@
 import "../styles/Header.css";
+import {FaBrain} from 'react-icons/fa';
+import { IconContext } from "react-icons";
 
 const Header = (props) => {
   return (
     <header>
       <div id="header-title">Memory Game</div>
+      <IconContext.Provider value={{size: '30px'}}>
+        <FaBrain />
+      </IconContext.Provider>
       <div id="header-scores">
         <div id="current-score">Score: {props.scores.current}</div>
         <div id="best-score">Best Score: {props.scores.best}</div>
